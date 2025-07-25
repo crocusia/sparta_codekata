0c18225e52e3b92a395dada0d1fd664bed7c464f
Subin Lee <rainbowsubin0522@gmail.com>
2025-07-25 23:08:00 +0900

    [level 2] Title: 전화번호 목록, Time: 71.25 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -9,12 +9,11 @@ bool solution(vector<string> phone_book) {
     
     sort(phone_book.begin(), phone_book.end());
     
-    for(int i = 0; i < phone_book.size()-1; i++){
-        string current = phone_book[i];
-        if(phone_book[i+1].compare(0, current.length(), current) == 0){
-            answer = false;
-            break;
-        }
+    for(int i = 0; i < phone_book.size() - 1; i++)
+    {
+
+        if(phone_book[i + 1].find(phone_book[i]) == 0)
+            return false;
     }
     
     return answer;
